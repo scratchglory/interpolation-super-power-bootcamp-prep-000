6edd3e763c48f51bb5e8f403f6d9208e072e27d5
Adella <adellakenn@gmail.com>
2019-05-17 14:33:11 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,12 +1,12 @@
 # Write your #display_rainbow method here
 require 'pry'
 def display_rainbow(array)
-  binding.pry
-  "R: #{array[0]}"
-  "O: #{array[1]}"
-  "Y: #{array[2]}"
-  "G: #{array[3]}"
-  "B: #{array[4]}"
-  "I: #{array[5]}"
-  "V: #{array[6]}"
+  # binding.pry
+  "R: #{array[0]},
+   O: #{array[1]},
+   Y: #{array[2]},
+   G: #{array[3]},
+   B: #{array[4]},
+   I: #{array[5]},
+   V: #{array[6]}"
 end
